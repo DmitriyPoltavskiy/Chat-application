43f36939ca8d2c4aae88e147f57c6b45b4a08203
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-14 23:31:44 +0200

    Finished alfa version

--- app/js/controllers/chatCtrl.js
+++ app/js/controllers/chatCtrl.js
@@ -1,5 +1,6 @@
-function chatCtrl(ChatService) {
+function chatCtrl(ChatService, $firebaseAuth) {
 	var vm = this;
+	var auth = $firebaseAuth();
 
 	// Получаем сообщения с сервера
 	vm.messages = ChatService.getMessages();
@@ -8,23 +9,47 @@ function chatCtrl(ChatService) {
 
 	// Берем из textarea текст сообщения 
 	vm.sendMessage = function() {
-		var message = {
-			text: vm.newMessage
-		}
+		if(vm.author != null)
+		{
+			var message = {
+				authorMessage: vm.author.displayName,
+				authorId: vm.author.uid,
+				authorPhoto: vm.author.photoURL,
+				text: vm.newMessage
+				// time: new Date().getTime()
+			}
 
-		if (vm.newMessage != "") {
-			ChatService.sendMessage(message); // Вызываем метод сервиса
-			vm.newMessage = ""; // Очищаем textarea после отправки
-			
-			$(".chat-footer-message").removeClass("absentText");
+			if (vm.newMessage != "") {
+				ChatService.sendMessage(message); // Вызываем метод сервиса
+				vm.newMessage = ""; // Очищаем textarea после отправки
+				
+				$(".chat-footer-message").removeClass("absentText");
+			}
+			else {
+				$(".chat-footer-message").focus();
+				$(".chat-footer-message").addClass("absentText");
+			}
 		}
 		else {
-			$(".chat-footer-message").focus();
-			$(".chat-footer-message").addClass("absentText");
+			alert("You are not login!");
 		}
 	}
+
+	vm.login = function() {
+		auth.$signInWithPopup("google");
+	}
+
+	vm.logout = function() {
+		auth.$signOut();
+	}
+
+	auth.$onAuthStateChanged(function(authData) {
+		console.log(authData);
+		vm.author = authData;
+	});
+
 }
 
 
 angular.module("chatApp")
-.controller("chatCtrl", ["ChatService", chatCtrl]);
+.controller("chatCtrl", ["ChatService", "$firebaseAuth", chatCtrl]);
