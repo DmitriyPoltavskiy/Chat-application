7e8071115adf873223e5878a120c176b41718da3
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-15 01:35:39 +0200

    Add auth with facebook, twitter and problem with github

--- app/js/controllers/chatCtrl.js
+++ app/js/controllers/chatCtrl.js
@@ -1,6 +1,10 @@
 function chatCtrl(ChatService, $firebaseAuth) {
 	var vm = this;
 	var auth = $firebaseAuth();
+	var providerF = new firebase.auth.FacebookAuthProvider();
+	var providerT = new firebase.auth.TwitterAuthProvider();
+	// var providerG = new firebase.auth.GithubAuthProvider();
+	var providerG = new firebase.auth.GithubAuthProvider();
 
 	// Получаем сообщения с сервера
 	vm.messages = ChatService.getMessages();
@@ -37,6 +41,39 @@ function chatCtrl(ChatService, $firebaseAuth) {
 
 	vm.login = function() {
 		auth.$signInWithPopup("google");
+	}
+
+	vm.loginF = function() {
+		auth.$signInWithPopup(providerF);
+	}
+
+	vm.loginT = function() {
+		auth.$signInWithPopup(providerT);
+	}
+
+	vm.loginG = function() {
+		auth.$signInWithPopup(providerG).then(function(result) {
+		  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+		  var token = result.credential.accessToken;
+		  console.log("token: " + token);
+		  // The signed-in user info.
+		  var user = result.user;
+		  console.log("user: " + user);
+		  // ...
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  console.log("errorCode: " + errorCode);
+		  var errorMessage = error.message;
+		  console.log("errorMessage: " + errorMessage);
+		  // The email of the user's account used.
+		  var email = error.email;
+		  console.log("email: " + email);
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  console.log("credential: " + credential);
+		  // ...
+		});
 	}
 
 	vm.logout = function() {
