248c4ea386b5fa8f3da5f047b876d95d85ffda79
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-16 14:56:38 +0200

    Error during login

--- app/js/app.js
+++ app/js/app.js
@@ -12,8 +12,9 @@ angular.module("chatApp", [
 	"firebase",
 	// "chatApp.home",
 	"ngRoute"
-]).
-config(["$routeProvider", function($routeProvider) {
+])
+
+.config(["$routeProvider", function($routeProvider) {
 	// назначаем представление по умолчанию
 	$routeProvider
 	.otherwise({
@@ -24,6 +25,53 @@ config(["$routeProvider", function($routeProvider) {
 		controller: "homeCtrl"
 	});
 }])
-.controller("homeCtrl", [function() {
 
-}]);
+.controller("homeCtrl",  [
+	"$scope", 
+	"$firebaseAuth",
+	function($scope, $firebaseAuth) {
+
+	// var vm = this;
+	
+	$scope.SignIn = function(e) {
+	e.preventDefault(); // предотвращаем перезагрузку страницы
+	
+	var username = $scope.user.email;
+	var password = $scope.user.password;
+	
+	console.log("username: " + username);
+	console.log("password: " + password);
+
+	var firebaseObj = new Firebase("https://blistering-heat-2473.firebaseio.com");
+	var loginObj = $firebaseAuth(firebaseObj);
+
+	loginObj.$authWithPassword({
+		email: username,
+		password: password
+	})
+	.then(function(user) {
+		//Success callback
+		console.log('Authentication successful');
+	}, function(error) {
+		//Failure callback
+		console.log('Authentication failure');
+	});
+
+}}]);
+
+// app.controller("SampleCtrl", ["$scope", "$firebaseAuth",
+//   function($scope, $firebaseAuth) {
+//     var auth = $firebaseAuth();
+
+//     $scope.signIn = function() {
+//       $scope.firebaseUser = null;
+//       $scope.error = null;
+
+//       auth.$signInAnonymously().then(function(firebaseUser) {
+//         $scope.firebaseUser = firebaseUser;
+//       }).catch(function(error) {
+//         $scope.error = error;
+//       });
+//     };
+//   }
+// ]);
