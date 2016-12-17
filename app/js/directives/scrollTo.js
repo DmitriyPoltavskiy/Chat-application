d084e344b454dde9ac3f3e2b13348a722328552a
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-18 01:11:59 +0200

    Add two directives for scroll and keybinding

--- app/js/directives/scrollTo.js
+++ app/js/directives/scrollTo.js
@@ -1,5 +1,5 @@
 angular.module("chatApp")
-.directive("scrollTo", [function($anchorScroll, $location) {
+.directive("scrollTo", function() {
 	return {
 		restrict: "A",
 		link: function() {
@@ -7,32 +7,6 @@ angular.module("chatApp")
 				var pos = document.getElementById("scrollHeight").scrollHeight;
 				$(".chat-body").scrollTo(pos);
 			});
-				// var elem = $(".chat-message:last-child");
-				// console.log("elem: " + elem);
-				// var pos = elem.scrollHeight;
-				// var pos = elem.scrollTop();
-				// console.log("pos: " + pos);
-				// elem.css("color", "red");
-
-				// elem.scrollIntoView(false);
-				// $location.hash(pos);
-
-				// $anchorScroll();
-				// alert("Im working!");
-
-
-				// var elem = $(".chat-message:last-child");
-				// elem.addClass("scrollTo");
-				// console.log("I am working");
-
-
-
-				// $(".scrollTo").scrollToID({
-				// 	speed: 1000, //expressed in Milliseconds
-				// 	offset: 300   //in pixels
-				// });
-
-
 		}
 	}
-}]);
+});
