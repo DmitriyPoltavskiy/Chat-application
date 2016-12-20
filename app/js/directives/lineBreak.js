77720b3624c04f3433edf1465257f184f686fdf9
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-20 21:31:11 +0200

    first stage

---
+++ app/js/directives/lineBreak.js
@@ -0,0 +1,20 @@
+angular.module("chatApp")
+.directive("lineBreak", function() {
+	// return {
+	// 	restrict: "A",
+	// 	link: function(scope, elements, attrs) {
+	// 		elements.keydown(function() {
+	// 			console.log("You press enter + shift");
+	// 		});
+	// 	}
+
+
+	// return function(scope, elements, attrs) {
+	// 	elements.bind("keydown keypress", function(event) {
+	// 		// if(event.which === 13 && event.which === 16) {
+	// 		// 	event.preventDefault();
+	// 		// 	console.log("You press enter + shift");
+	// 		// }
+	// 	});
+	// }
+});
--- app/js/directives/sendDirective.js
+++ app/js/directives/sendDirective.js
@@ -1,14 +1,65 @@
 function clickToSend() {
 	return function(scope, elements, attrs) {
-		elements.bind("keydown keypress", function(event) {
-			if(event.which === 13) {
+
+		// var enter = false;
+		// var shift = true;
+
+		// elements.keydown(function(event) {
+		// 	// event.preventDefault();
+		// 	if(event.which == 13) {
+		// 		enter = true;
+		// 		console.log("Yes enter");
+		// 	}
+		// });
+
+		// elements.keydown(function(event) {
+		// 	if(event.which == 16) {
+		// 		shift = true;
+		// 		console.log("Yes shift");
+		// 	}
+		// });
+
+		elements.on('keydown', function(event){
+			if (event.keyCode == 16) {
+				$(document).on('keydown', function(event){
+					if (event.keyCode == 13) {
+						$(document).off('keydown');
+						// shift = false;
+						console.log("Yes shift + enter");
+					}
+				})
+				.keyup(function(event){
+					// shift = true;
+				});
+			}
+			else if(event.keyCode == 13) {
 				scope.$apply(function() {
 					scope.$eval(attrs.sendDirective)
 				});
-
 				event.preventDefault();
 			}
 		});
+
+
+
+
+		// if(event.which === 13) {
+		// 	scope.$apply(function() {
+		// 		scope.$eval(attrs.sendDirective)
+		// 	});
+		// }
+
+
+
+
+		// elements.bind("keydown keypress", function(event) {
+		// 	if(event.which === 13) {
+		// 		scope.$apply(function() {
+		// 			scope.$eval(attrs.sendDirective)
+		// 		});
+		// 		event.preventDefault();
+		// 	}
+		// });
 	}
 }
 
