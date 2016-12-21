a0e9670570e3174345c7496e52ea021451ec97e8
Dmitriy Poltavskiy <di77929@gmail.com>
2016-12-21 22:43:58 +0200

    Fix problem with database

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,13 @@ var config = {
 	 storageBucket: "chatapp-2b28b.appspot.com",
 	 messagingSenderId: "577030966423"
 };
+// var config = {
+//     apiKey: "AIzaSyBJ_1WgGn42OQq7V9QbIzMB2P5s_hAsMAM",
+//     authDomain: "chat-cat-1818d.firebaseapp.com",
+//     databaseURL: "https://chat-cat-1818d.firebaseio.com",
+//     storageBucket: "chat-cat-1818d.appspot.com",
+//     messagingSenderId: "385006392568"
+//   };
 firebase.initializeApp(config);
 
 angular.module("chatApp", [
